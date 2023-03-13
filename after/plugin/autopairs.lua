dab3bf6b600d71c4e1800ee92da6cf6b8e4d434b
Justin D'Errico <justin.derrico1991@gmail.com>
2023-03-13 15:44:02 -0400

    autopairs fix

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -2,6 +2,11 @@ local npairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
 
 npairs.setup({
+	enable_check_bracket_line = false,
+})
+npairs.enable()
+
+npairs.setup({
 	check_ts = true,
 	ts_config = {
 		lua = { "string" }, -- it will not add a pair on that treesitter node
