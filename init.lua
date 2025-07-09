c670b1379f41931f720fff2579b1a6599e96f539
Justin D'Errico <justin.derrico1991@gmail.com>
2025-07-09 19:02:41 -0400

    update deps

--- init.lua
+++ init.lua
@@ -91,7 +91,11 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
 			-- Snippets
-			{ "L3MON4D3/LuaSnip" }, -- Required
+			{
+				"L3MON4D3/LuaSnip",
+				-- follow latest release.
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			},
 			{ "rafamadriz/friendly-snippets" }, -- Optional
 		},
 	},
